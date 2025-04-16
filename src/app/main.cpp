08258f2b07e5a3b2b6647a826eb6ab9685eae23c
hinata <hinatachikao1971@gmail.com>
2025-04-17 01:37:21 +0330

    new cpp source files

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,14 +1,10 @@
-#include "../header/utillib.hpp"
-#include <filesystem>
-#include <iostream>
+/*   */
+
+#include "main.hpp"
+
 
 int main() {
 
-
-    // const std::filesystem::__cxx11::path cp = std::filesystem::current_path();
-    const auto cp = std::filesystem::current_path();
-    std::cout << "current path is: " << cp << NL;
-
-    std::cout << "\n #(10:40:55): The End ..." << std::endl;
-    return (EXIT_SUCCESS);
+    out << "\n #(00:00:00): The End ..." << end;
+    return EXIT_SUCCESS;
 }
